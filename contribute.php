3cd82e48bbe3ede05b46c5f17ea524935b92ef32
Martin Bonnin <mbo@netgem.com>
2014-03-27 13:42:21 +0100

    add a reference to the google group

--- contribute.php
+++ contribute.php
@@ -6,6 +6,7 @@
             <p>We welcome any new format implemented using the <a href="hmdoc.php">hmscript</a> language. For an idea on how to do this, take a look at the <a href="https://github.com/HexaMonkey/hexamonkey/tree/master/scripts">existing scripts</a>. You can just drop your script in your ~/.hexamonkey directory and they will be taken into account next time you compile.</p>
             <p>There is also a lot to do on the core compiler and on the UI side. Please contact us if you want to help, we have plenty of ideas.</p>
             <h2>Contact</h2>
-            <p>The easiest way to contact us at this point is through github issues and pull request.</p>
+            <p>You can ask all kind of questions on the hexamonkey <a href="https://groups.google.com/forum/#!forum/hexamonkey">google group</a>.</p>
+            <p>We accept bugs and pull request directly on <a href="http://github.com/hexamonkey">github</a>.</p>
             <p>We also sometimes hang out on irc, channel #hexamonkey on irc.freenode.net, you might see us there from time to time.</p>
 <?php require_once("footer.php"); ?>
